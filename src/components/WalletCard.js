cf2ca5759c8c0fd6efcb2a44a385da4b903799b5
Alaa <alaa.maghrabi.m@gmail.com>
2022-04-08 16:14:05 +0200

    Start of the project

---
+++ src/components/WalletCard.js
@@ -0,0 +1,125 @@
+import React, { useEffect, useState } from "react"
+import CustomBtn from './CustomBtn';
+import {Typography} from '@material-ui/core'
+import {makeStyles} from "@material-ui/core/styles"; 
+
+const styles = makeStyles({
+    container:{
+        position: "relative",
+        display: "inline-block",
+    },
+    dropdown:{
+        position: "absolute",
+        right: "0",
+        left: "0",
+        width: "168px",
+        zIindex: "2",
+    },
+    
+    ul_style:{
+        listStyle: "none",
+        padding: "0",
+        margin: "0",
+    },
+
+    li_style:{
+        padding: "10px 12px",
+        cursor: "pointer",
+        background: 'rgba(0,0,0,0.25)',
+        '&:hover': {
+            background: '#3498db'
+          }
+    },
+
+})
+
+const Moralis = require('moralis');
+
+const WalletCard = () =>{
+
+    const classes = styles();
+
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+    const [defaultAccount, setDefaultAccount] = useState(null);
+	const [userBalance, setUserBalance] = useState(null);
+    const [showResults, setShowResults] = useState(false)
+
+    // Connect the Moralis server
+    const serverUrl = "https://7wqjkn4step0.usemoralis.com:2053/server";
+    const appId = "mMGEOLWm7x7ayKaRgIdoNWsreOKAHL0Ulu17qZ8I";
+
+    Moralis.start({ serverUrl, appId });
+
+    /* Authentication code */
+    async function login() {
+        let user = Moralis.User.current();
+        if (!user) {
+        user = await Moralis.authenticate({ signingMessage: "Log in using Moralis" })
+            .then(function (user) {
+                console.log("logged in user:", user);
+                console.log(user.get("ethAddress"));
+            })
+            .catch(function (error) {
+            console.log(error);
+            });
+        }
+        accounthandler(user)
+        setShowResults(!showResults);
+    }
+
+    async function accounthandler(account) {
+        console.log(account);
+        const acc_address = account.get("ethAddress");
+        setDefaultAccount(acc_address);
+        
+        const balance = await Moralis.Web3API.account.getNativeBalance({chain: "rinkeby"}); 
+        setUserBalance((balance.balance/10**18).toFixed(3));
+        
+        setConnButtonText((defaultAccount.substring(0,6)).concat(
+            " . . . ", 
+            (defaultAccount.substring(defaultAccount.length-6,defaultAccount.length))
+        ));               
+    }
+
+
+    async function logOut() {
+        await Moralis.User.logOut();
+        console.log("logged out");
+        setConnButtonText('Connect Wallet');
+    }
+    
+
+
+    return ( 
+        <div className={classes.container}>
+			<CustomBtn onClick={login} txt={connButtonText}/>
+            {showResults && (
+                <div className={classes.dropdown}>
+                    <ul className={classes.ul_style}>
+                        <li className={classes.li_style}>
+                            Option 1
+                        </li>
+                        <li className={classes.li_style}>
+                            Option 2
+                        </li>
+                        <li className={classes.li_style}>
+                            Option 3
+                        </li>
+                        <li className={classes.li_style}>
+                            <div onClick={logOut}>Log Out</div>
+                        </li>
+                    </ul>
+                </div>
+            )}
+
+        </div>
+     )
+}
+           /* <Typography variant="h6" className='accountDisplay'>
+				Address: {defaultAccount}
+			</Typography>
+            <Typography variant="h6" className='balanceDisplay'>
+				Balance: {userBalance}
+			</Typography>*/
+export default WalletCard;
